c430ac2a3de7f8460e4ebc6f75fed574c02dda97
Sanju Devadas <sanjudevadas11@gmail.com>
2024-07-10 12:11:56 +0530

    Complted the total setup little works pending

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,18 +4,19 @@ import { assets } from "../../assets/assets";
 import { Context } from "../../context/Context";
 
 const Main = () => {
-
-const {prevprompt,
-  setPrevprompt,
-  onSent,
-  setRecentprompt,
-  recentprompt,
-  showresult,
-  loading,
-  resultdata,
-  input,
-  setInput,
-  newChat}= useContext(Context)
+  const {
+    prevprompt,
+    setPrevprompt,
+    onSent,
+    setRecentprompt,
+    recentprompt,
+    showresult,
+    loading,
+    resultdata,
+    input,
+    setInput,
+    newChat,
+  } = useContext(Context);
 
   return (
     <div className="main">
@@ -24,61 +25,89 @@ const {prevprompt,
         <img src={assets.user_icon} alt="" />
       </div>
       <div className="maincontainer">
-        <div className="greet">
-          <p>
-            <span>Hello,Sanju</span>
-          </p>
-          <p>How can I Help you Today</p>
-        </div>
-        <div className="cards">
+        {!showresult ? (
+          <>
+            <div className="greet">
+              <p>
+                <span>Hello,Sanju</span>
+              </p>
+              <p>How can I Help you Today</p>
+            </div>
+            <div className="cards">
+              <div className="card">
+                <p>
+                  dont regret..dont fall.. you will rise and shine just wait for
+                  your moment
+                </p>
+                <img src={assets.compass_icon} alt="" />
+              </div>
 
-      
-        <div className="card">
-          <p>
-            dont regret..dont fall.. you will rise and shine just wait for your
-            moment
-          </p>
-          <img src={assets.compass_icon} alt="" />
-        </div>
+              <div className="card">
+                <p>
+                  Hey you wassup long time no see i was thinking about you
+                  yesterday
+                </p>
+                <img src={assets.bulb_icon} alt="" />
+              </div>
 
-        <div className="card">
-          <p>
-            Hey you wassup long time no see i was thinking about you yesterday
-          </p>
-          <img src={assets.bulb_icon} alt="" />
-        </div>
+              <div className="card">
+                <p>
+                  Do you remember the place and moment when we first met i think
+                  this is that place
+                </p>
+                <img src={assets.message_icon} alt="" />
+              </div>
 
-        <div className="card">
-          <p>
-            Do you remember the place and moment when we first met i think this
-            is that place
-          </p>
-          <img src={assets.message_icon} alt="" />
-        </div>
+              <div className="card">
+                <p>
+                  Injuries wont decide you whether you are good or not dont
+                  worry no matter what i will always support you
+                </p>
+                <img src={assets.code_icon} alt="" />
+              </div>
+            </div>
+          </>
+        ) : (
+          <div className="result">
+            <div className="result-title">
+              <img src={assets.user_icon} alt="" />
+              <p>{recentprompt}</p>
+            </div>
+            <div className="result-data">
+              <img src={assets.gemini_icon} alt="" />
+              {loading ? (
+                <div className="loader">
+                  <hr />
+                  <hr />
+                  <hr />
+                </div>
+              ) : (
+                <p dangerouslySetInnerHTML={{ __html: resultdata }}></p>
+              )}
+            </div>
+          </div>
+        )}
 
-        <div className="card">
-          <p>
-            Injuries wont decide you whether you are good or not dont worry no
-            matter what i will always support you
-          </p>
-          <img src={assets.code_icon} alt="" />
-        </div>
-      </div>
-      <div className="mainbottom">
-        <div className="searchbox">
-            <input type="text" placeholder="Enter a prompt here" />
-          
+        <div className="mainbottom">
+          <div className="searchbox">
+            <input
+              onChange={(e) => setInput(e.target.value)}
+              value={input}
+              type="text"
+              placeholder="Enter a prompt here"
+            />
+
             <div>
-            <img src={assets.gallery_icon} alt="" />
-            <img src={assets.mic_icon} alt="" />
-            <img src={assets.send_icon} alt="" />
+              <img src={assets.gallery_icon} alt="" />
+              <img src={assets.mic_icon} alt="" />
+              <img onClick={() => onSent()} src={assets.send_icon} alt="" />
+            </div>
+          </div>
+          <p className="bottominfo">
+            Gemini may display inaccurate info, including about people, so
+            double-check its responses.
+          </p>
         </div>
-        </div>
-        <p className="bottominfo">
-        Gemini may display inaccurate info, including about people, so double-check its responses.
-         
-        </p>
-      </div>
       </div>
     </div>
   );
--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,48 +1,66 @@
 import { createContext, useState } from "react";
-import run from '../Config/gemini'
+import run from "../Config/gemini";
 
-export const Context = createContext()
+export const Context = createContext();
 
 const ContextProvider = (props) => {
-    const [input,setInput]= useState("");
-    const [recentprompt,setRecentprompt]=useState("");
-    const [prevprompt,setPrevprompt]=useState([]);
-    const [showresult,setShowresult]=useState(false);
-    const [loading,setLoading]=useState(false);
-    const[resultdata,setResultdata]=useState("")
+  const [input, setInput] = useState("");
+  const [recentprompt, setRecentprompt] = useState("");
+  const [prevprompt, setPrevprompt] = useState([]);
+  const [showresult, setShowresult] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [resultdata, setResultdata] = useState("");
 
+  const delayPara = (index, nextWord) => {
+setTimeout(function(){
+setResultdata(prev=>prev+nextWord)
+},75*index)
 
+  };
 
-    const onSent = async(prompt) => {
-
-     
-        await run(prompt)
-      
-    }
-   
-
-
-     const contextValue = {
-        prevprompt,
-        setPrevprompt,
-        onSent,
-        setRecentprompt,
-        recentprompt,
-        showresult,
-        loading,
-        resultdata,
-        input,
-        setInput,
-        newChat
-
-        
+  const onSent = async (prompt) => {
+    setResultdata("");
+    setLoading(true);
+    setShowresult(true);
+    setRecentprompt(input);
+    const response = await run(input);
+    let responseArray = response.split("**");
+    let newResponse;
+    for(let i=0;i <responseArray.length;i++){
+     if(i ===0 || i%2 !== 1){
+        newResponse +=responseArray[i];
+     }
+     else{
+        newResponse += "<b>"+responseArray[i]+"</b>";
      }
 
-    return(
-        <Context.Provider value = {contextValue}>
-         {props.children}
-        </Context.Provider>
-    )
-}
+    }
+    let newResponse2= newResponse.split("*").join("</br>")
+    let newResponseArray = newResponse2.split(" ");
+    for(let i=0; i<newResponseArray.length; i++){
+     const nextWord = newResponseArray[i]
+     delayPara(i,nextWord+" ")
+    }
+    setLoading(false);
+    setInput("");
+  };
 
-export default ContextProvider 
+  const contextValue = {
+    prevprompt,
+    setPrevprompt,
+    onSent,
+    setRecentprompt,
+    recentprompt,
+    showresult,
+    loading,
+    resultdata,
+    input,
+    setInput,
+  };
+
+  return (
+    <Context.Provider value={contextValue}>{props.children}</Context.Provider>
+  );
+};
+
+export default ContextProvider;
